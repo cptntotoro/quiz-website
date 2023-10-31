1716ef5762b90dd084740043613a40b42ebd13f0
Anastasia <bitstasia@gmail.com>
2023-10-31 19:36:54 +0300

    Develop quiz popup. Results popup in progress

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Which is the smallest planet within our solar system?",
+        answer: "B. Mercury",
+        options: [
+            "A. Mars",
+            "B. Mercury",
+            "C. Venus",
+            "D. Uranus"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Which is the brightest planet in the night’s sky?",
+        answer: "D. Venus",
+        options: [
+            "A. Mercury",
+            "B. Juputer",
+            "C. Uranus",
+            "D. Venus"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Which is the oldest planet in our solar system?",
+        answer: "A. Juputer",
+        options: [
+            "A. Juputer",
+            "B. Venus",
+            "C. Mercury",
+            "D. Saturn"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Which planet spins backward relative to the others?",
+        answer: "C. Venus",
+        options: [
+            "A. Mercury",
+            "B. Mars",
+            "C. Venus",
+            "D. Juputer"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Which planet has the most moons?",
+        answer: "D. Saturn",
+        options: [
+            "A. Mars",
+            "B. Mercury",
+            "C. Venus",
+            "D. Saturn"
+        ]
+    },
+]
--- script.js
+++ script.js
@@ -2,6 +2,12 @@ const startBtn = document.querySelector('.start-btn');
 const popupInfo = document.querySelector('.popup-info');
 const exitBtn = document.querySelector('.exit-btn');
 const main = document.querySelector('.main');
+const continueBtn = document.querySelector('.continue-btn');
+const quizSection = document.querySelector('.quiz-section');
+const quizBox = document.querySelector('.quiz-box');
+const nextBtn = document.querySelector('.next-btn');
+const optionList = document.querySelector('.option-list');
+const resultBox = document.querySelector('.result-box');
 
 startBtn.onclick = () => {
     popupInfo.classList.add('active');
@@ -11,4 +17,98 @@ startBtn.onclick = () => {
 exitBtn.onclick = () => {
     popupInfo.classList.remove('active');
     main.classList.remove('active');
+}
+
+continueBtn.onclick = () => {
+    quizSection.classList.add('active');
+    popupInfo.classList.remove('active');
+    main.classList.remove('active');
+    quizBox.classList.add('active');
+
+    displayQuestions(0);
+    questionCounter(1);
+    updateHeaderScore();
+}
+
+let questionCount = 0;
+let questionNumb = 1;
+let userScore = 0;
+
+nextBtn.onclick = () => {
+    if (questionCount < questions.length - 1) {
+        questionCount++;
+        displayQuestions(questionCount);
+
+        questionNumb++;
+        questionCounter(questionNumb);
+
+        nextBtn.classList.remove('active');
+    } else {
+        displayResultBox();
+    }
+}
+
+// getting questions and options from array
+
+function displayQuestions(index) {
+    const questionText = document.querySelector('.question-text');
+    questionText.textContent = `${questions[index].numb}. ${questions[index].question}`;
+
+    let optionTag = `<div class="option"><span>${questions[index].options[0]}</span></div>
+                    <div class="option"><span>${questions[index].options[1]}</span></div>
+                    <div class="option"><span>${questions[index].options[2]}</span></div>
+                    <div class="option"><span>${questions[index].options[3]}</span></div>`
+
+    optionList.innerHTML = optionTag;
+
+    const option = document.querySelectorAll('.option');
+
+    for (let i = 0; i < option.length; i++) {
+        option[i].setAttribute('onclick', 'checkAnswer(this)');
+    }
+}
+
+function checkAnswer(answer) {
+    let userAnswer = answer.textContent;
+    let correctAnswer = questions[questionCount].answer;
+    let allOptions = optionList.children.length;
+
+    if (userAnswer == correctAnswer) {
+        answer.classList.add('correct');
+        userScore++;
+        updateHeaderScore();
+    } else {
+        answer.classList.add('incorrect');
+
+        // if selected option is incorrect, highlight the right one too
+
+        for (let i = 0; i < allOptions; i++) {
+            if (optionList.children[i].textContent == correctAnswer) {
+                optionList.children[i].setAttribute('class', 'option correct');
+            }
+        }
+    }
+
+    // after user selects an option, disable the rest
+
+    for (let i = 0; i < allOptions; i++) {
+        optionList.children[i].classList.add('disabled');
+    }
+
+    nextBtn.classList.add('active');
+}
+
+function questionCounter(index) {
+    const questionsTotal = document.querySelector('.questions-total');
+    questionsTotal.textContent = `${index} of ${questions.length} Questions`;
+}
+
+function updateHeaderScore() {
+    const headerScoreText = document.querySelector('.header-score');
+    headerScoreText.textContent = `Score: ${userScore} / ${questions.length}`;
+}
+
+function displayResultBox() {
+    quizBox.classList.remove('active');
+    resultBox.classList.add('active');
 }
